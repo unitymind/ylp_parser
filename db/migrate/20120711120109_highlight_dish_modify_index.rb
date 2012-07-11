5cb7c3e07ab807bdf355427f79d1a8fb17a1d20c
Vitaliy V. Shopov <unitymind.apps@gmail.com>
2012-07-11 16:10:27 +0400

    HighlightDishModifyIndex fix

--- db/migrate/20120711120109_highlight_dish_modify_index.rb
+++ db/migrate/20120711120109_highlight_dish_modify_index.rb
@@ -1,6 +1,6 @@
 class HighlightDishModifyIndex < ActiveRecord::Migration
   def change
     remove_index :yelp_highlight_dishes, [:sentence_review_id]
-    add_index :yelp_highlight_dishes, [:restaurant_id, :sentence_review_id], :unique => true
+    add_index :yelp_highlight_dishes, [:restaraunt_id, :sentence_review_id], :name => :index_sentence_review_id_by_restaurant_unique, :unique => true
   end
 end
